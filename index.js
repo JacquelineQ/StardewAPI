1a4ac6063c9fa22d2afb40c8f400199b4fe8a58e
Jacqueline <quackenbushjacqueline@gmail.com>
2022-12-09 18:12:13 -0500

    issue filtering by name

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const app = express();
 //Gets all Villagers
 app.get('/api/villagers', (req, res) => {
     res.json(villagers);
-})
+});
 
+//Get single Villager by Name
+app.get('/api/villagers/:name', (req, res) => {
+    res.json(villagers.filter(villager => villagers.name === req.params.name));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
