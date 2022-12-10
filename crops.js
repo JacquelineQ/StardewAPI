65e24bf2727311bcfbecd2b34186654d81936769
Jacqueline <quackenbushjacqueline@gmail.com>
2022-12-10 01:01:57 -0500

    ++crop skeleton outline

--- crops.js
+++ crops.js
@@ -1,5 +1,12 @@
 const crops = [
     //Get info from Cookng section of wiki
+    //Need to decide what to do with notes
+{
+    name: "",
+    season: "",
+    growTime: "",
+    imgSrc: ""
+}
 ];
 
 module.exports = crops;
