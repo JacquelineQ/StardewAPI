314d12f5d5841e70c3e3c7ee39a080f11b5a7709
Jacqueline <quackenbushjacqueline@gmail.com>
2022-12-10 00:13:10 -0500

    ++artifact names

--- artifacts.js
+++ artifacts.js
@@ -1,11 +1,293 @@
 const artifacts = [
+    //42
+    //Do I want to include percentages in location? Set as array or object?
 {
-    name: "",
+    name: "Ambhibian-Fossil",
+    description: "The relatively short hind legs suggest some kind of primordial toad.",
+    sellPrice: "150g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/c/cf/Amphibian_Fossil.png"
+},
+{
+    name: "Anchor",
+    description: "It may have belonged to ancient pirates.",
+    sellPrice: "100g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/c/c0/Anchor.png"
+},
+{
+    name: "Ancient-Doll",
+    description: "An ancient doll covered in grime. This doll may have been used as a toy, a decoration, or a prop in some kind of ritual.",
+    sellPrice: "60g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/c/c0/Ancient_Doll.png"
+},
+{
+    name: "Ancient-Drum",
+    description: "It's a drum made from wood and animal skin. It has a low, reverberating tone.",
+    sellPrice: "100g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/1/1e/Ancient_Drum.png"
+},
+{
+    name: "Ancient-Seed",
+    description: "It's a dry old seed from some ancient plant. By all appearances it's long since dead...",
+    sellPrice: "5g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/a/af/Ancient_Seed.png"
+},
+{
+    name: "Ancient-Sword",
+    description: "It's the remains of an ancient sword. Most of the blade has turned to rust, but the hilt is very finely crafted.",
+    sellPrice: "100g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/7/76/Ancient_Sword.png"
+},
+{
+    name: "Arrowhead",
+    description: "A crudely fashioned point used for hunting.",
+    sellPrice: "40g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/d/d1/Arrowhead.png"
+},
+{
+    name: "Bone-Flute",
+    description: "It's a prehistoric wind instrument carved from an animal's bone. It produces an eerie tone.",
+    sellPrice: "100g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/f/fc/Bone_Flute.png"
+},
+{
+    name: "Chewing-Stick",
+    description: "Ancient people chewed on these to keep their teeth clean.",
+    sellPrice: "50g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/d/d2/Chewing_Stick.png"
+},
+{
+    name: "Chicken-Statue",
+    description: "It's a statue of a chicken on a bronze base. The ancient people of this area must have been very fond of chickens.",
+    sellPrice: "50g",
+    location: {},
+    imgSrc: "https://stardewvalleywiki.com/mediawiki/images/a/af/Chicken_Statue.png"
+},
+{
+    name: "Chipped-Amphora",
     description: "",
     sellPrice: "",
-    location: [],
+    location: {},
     imgSrc: ""
 },
+{
+    name: "Dinosaur-Egg",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dried-Starfish",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarf-Gadget",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarf-Scroll-I",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarf-Scroll-II",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarf-Scroll-III",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarf-Scroll-IV",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Dwarvish-Helm",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Elvish-Jewelry",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Glass Shards",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Golden-Mask",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Golden-Relic",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Nautilus-Fossil",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Palm-Fossil",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Handaxe",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Rib",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Scapula",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Skull",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Tibia",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Tool",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Prehistoric-Vertebra",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Rare-Disc",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Rusty-Cog",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Rusty-Spoon",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Rusty-Spur",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Skeletal-Hand",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Skeletal-Tail",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Strange-Doll", //Not sure how to distinguish the two different strange dolls with the same name
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Strange-Doll",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+},
+{
+    name: "Trilobite",
+    description: "",
+    sellPrice: "",
+    location: {},
+    imgSrc: ""
+}
 ];
 
 module.exports = artifacts;
