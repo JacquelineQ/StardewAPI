c52fb02e78558d3fd17a150859a748a1090d1997
Jacqueline <quackenbushjacqueline@gmail.com>
2022-12-19 19:56:25 -0500

    +achievements.js & data skeleton

---
+++ achievements.js
@@ -0,0 +1,12 @@
+// Achievements that do not have an in-game icon are Steam-specific achievements.
+//Will have to change Unlocks strings to references to hats.js table
+
+const achievements = [
+{
+    name: "",
+    description: "",
+    unlocks: "",
+    steamIcon: "",
+    gameIcon: ""
+},
+];
